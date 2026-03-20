b03dc93f03abf33a082b19f04eed14540dc54590
rachithrao2010 <rachithrao2010@gmail.com>
2026-03-20 11:47:54 -0500

    Added Icon to Window

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("WebUI Launcher")
-root.geometry("300x200")
+root.geometry("1000x600")
+icon = tk.PhotoImage(file="WindowIcon.png")
+root.iconphoto(False, icon)
+root.mainloop()
