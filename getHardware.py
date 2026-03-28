d5ed2e080fa3750f6f005227e8bdd1d5cacefde9
rachithrao2010 <rachithrao2010@gmail.com>
2026-03-28 12:10:39 -0500

    impplemented getCPUScore()

--- getHardware.py
+++ getHardware.py
@@ -1,4 +1,6 @@
 import subprocess
+import multiprocessing
+from cpuinfo import get_cpu_info
 CPU_corecount = 0
 CPU_maxclockspeed = 0
 CPU_L3Cachesize = 0
@@ -39,15 +41,43 @@ def getOverallScore():
     pass
 
 def getCPUScore():
-    global CPU_corecount, CPU_L3Cachesize, CPU_Threadcount, CPU_AVX512, CPU_AMX
-    cmd = "wmic cpu get NumberOfCores"
-    output = subprocess.check_output(cmd, shell=True, creationflags=0x08000000).decode().strip().split()
-    CPU_corecount = int(output[1])
+    global CPU_corecount, CPU_L3Cachesize, CPU_Threadcount, CPU_maxclockspeed, CPU_AVX512, CPU_AMX
 
-    cmd = "wmic cpu get NumberOfLogicalProcessors"
-    output = subprocess.check_output(cmd, shell=True, creationflags=0x08000000).decode().strip().split()
-    CPU_Threadcount = int(output[1])
+    cmd = "(Get-CimInstance -ClassName Win32_Processor | Measure-Object -Property NumberOfCores -Sum).Sum"
+    corecount = subprocess.run(["powershell", "-Command", cmd], capture_output=True, text=True, check=True).stdout.split()
+    CPU_corecount = int(corecount[0])
 
+    cmd = "(Get-CimInstance -ClassName Win32_Processor | Measure-Object -Property NumberOfLogicalProcessors -Sum).Sum"
+    threadcount = subprocess.run(["powershell", "-Command", cmd], capture_output=True, text=True, check=True).stdout.split()
+    CPU_Threadcount = int(threadcount[0])
+
+    cmd = "(Get-CimInstance Win32_Processor | Select-Object Name, L3CacheSize)"
+    cachesize = subprocess.run(["powershell", "-Command", cmd], capture_output=True, text=True, check=True).stdout.split()
+    CPU_L3Cachesize = int(cachesize[-1])/1000
+
+    cmd = "(Get-CimInstance Win32_Processor).MaxClockSpeed"
+    clkspd = subprocess.run(["powershell", "-Command", cmd], capture_output=True, text=True, check=True).stdout.split()
+    CPU_maxclockspeed = int(clkspd[0]) / 1000
+
+    flags = get_cpu_info().get('flags', [])
+    
+
+    CPU_AVX512 = any('avx512' in flag for flag in flags)
+
+    CPU_AMX = any('amx' in flag for flag in flags)
+
+    score = 0
+    score += min(100, max(0, ((CPU_corecount / 15) * 100))) * 0.2
+    score += min(100, max(0, ((CPU_Threadcount / CPU_corecount) * 100))) * 0.03
+    score += min(100, max(0, ((CPU_L3Cachesize / 64) * 100))) * 0.1
+    score += min(100, max(0, ((CPU_maxclockspeed / 3.5) * 100))) * 0.07
+    if CPU_AMX:
+        score += 35
+    if CPU_AVX512:
+        score += 25
+    
+    return score
+    
     
 def updateHardware(cpu, gpu, ram):
     cpu.config(text="CPU: " + getCPU())
@@ -63,6 +93,6 @@ def getRAMScore():
 def writeScores():
     pass
 
-getCPUScore()
+print(getCPUScore())
 
 
--- main.py
+++ main.py
@@ -4,68 +4,73 @@ import sv_ttk
 import getHardware
 from pathlib import Path
 import threading
+import multiprocessing
 
-root = tk.Tk()
-root.title("WebUI Launcher")
-root.geometry("1000x600")
-icon = tk.PhotoImage(file="WindowIcon.png")
-root.iconphoto(False, icon)
+if __name__ == '__main__':
 
-sv_ttk.set_theme("dark")
-Notebookstyle = ttk.Style()
+    multiprocessing.freeze_support()
 
-Notebookstyle.layout("TNotebook.Tab", [
-    ('Notebook.tab', {
-        'sticky': 'nswe', 
-        'children': [
-            ('Notebook.padding', {
-                'side': 'top', 
-                'sticky': 'nswe', 
-                'children': [
-                    ('Notebook.label', {'side': 'top', 'sticky': ''})
-                ]
-            })
-        ]
-    })
-])
+    root = tk.Tk()
+    root.title("WebUI Launcher")
+    root.geometry("1000x600")
+    icon = tk.PhotoImage(file="WindowIcon.png")
+    root.iconphoto(False, icon)
 
-launchbuttonstyle = ttk.Style()
+    sv_ttk.set_theme("dark")
+    Notebookstyle = ttk.Style()
 
-launchbuttonstyle.configure('Launch.TButton', background='green', font=('Helvetica', 12, 'bold'))
+    Notebookstyle.layout("TNotebook.Tab", [
+        ('Notebook.tab', {
+            'sticky': 'nswe', 
+            'children': [
+                ('Notebook.padding', {
+                    'side': 'top', 
+                    'sticky': 'nswe', 
+                    'children': [
+                        ('Notebook.label', {'side': 'top', 'sticky': ''})
+                    ]
+                })
+            ]
+        })
+    ])
 
+    launchbuttonstyle = ttk.Style()
 
-notebook = ttk.Notebook()
-
-launch = ttk.Frame(notebook)
-recs = ttk.Frame(notebook, padding = 10)
-discover = ttk.Frame(notebook)
-settings = ttk.Frame(notebook)
-
-notebook.add(launch, text="Launch")
-
-launchbutton = ttk.Button(launch, style='Launch.TButton', text="LAUNCH")
-launchbutton.pack(pady=10, anchor="w")
-notebook.add(recs, text="Recommendations")
-notebook.add(discover, text="Discover")
-notebook.add(settings, text="Settings")
+    launchbuttonstyle.configure('Launch.TButton', background='green', font=('Helvetica', 12, 'bold'))
 
 
+    notebook = ttk.Notebook()
 
-Hardware = ttk.LabelFrame(recs, text="Hardware", padding=10)
-Hardware.pack(fill="x", pady=(0, 10))
-labelCPU = ttk.Label(Hardware, text="CPU: ")
-labelCPU.pack(pady=10, anchor="w")
-labelGPU = ttk.Label(Hardware, text="GPU: ")
-labelGPU.pack(pady=10, anchor="w")
-labelRAM = ttk.Label(Hardware, text="RAM: ")
-labelRAM.pack(pady=10, anchor="w")
+    launch = ttk.Frame(notebook)
+    recs = ttk.Frame(notebook, padding = 10)
+    discover = ttk.Frame(notebook)
+    settings = ttk.Frame(notebook)
 
-thread = threading.Thread(target=getHardware.updateHardware, args=(labelCPU, labelGPU, labelRAM))
-thread.daemon = True
-thread.start()
+    notebook.add(launch, text="Launch")
 
-
-notebook.pack(fill="both", expand=True)
+    launchbutton = ttk.Button(launch, style='Launch.TButton', text="LAUNCH")
+    launchbutton.pack(pady=10, anchor="w")
+    notebook.add(recs, text="Recommendations")
+    notebook.add(discover, text="Discover")
+    notebook.add(settings, text="Settings")
 
 
-root.mainloop()
+
+    Hardware = ttk.LabelFrame(recs, text="Hardware", padding=10)
+    Hardware.pack(fill="x", pady=(0, 10))
+    labelCPU = ttk.Label(Hardware, text="CPU: ")
+    labelCPU.pack(pady=10, anchor="w")
+    labelGPU = ttk.Label(Hardware, text="GPU: ")
+    labelGPU.pack(pady=10, anchor="w")
+    labelRAM = ttk.Label(Hardware, text="RAM: ")
+    labelRAM.pack(pady=10, anchor="w")
+
+    thread = threading.Thread(target=getHardware.updateHardware, args=(labelCPU, labelGPU, labelRAM))
+    thread.daemon = True
+    thread.start()
+
+
+    notebook.pack(fill="both", expand=True)
+
+
+    root.mainloop()
